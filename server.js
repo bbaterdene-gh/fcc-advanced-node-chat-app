ac2ec1b605fb0f348d656976eb50721c1dcb20f8
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2021-09-29 21:05:27 +0800

    Use a Template Engine's Powers

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.set('view engine', 'pug')
 
 
 app.route('/').get((req, res) => {
-  res.render('index');
+  res.render('index', {title: 'Hello', message: 'Please login'});
 });
 
 const PORT = process.env.PORT || 3000;
