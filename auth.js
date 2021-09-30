676bd4699a4874a5a6738290a657417739ea826b
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2021-09-30 17:14:53 +0800

    Implementation of Social Authentication II

--- auth.js
+++ auth.js
@@ -36,30 +36,9 @@ module.exports = function (app, myDataBase) {
     clientID: process.env.GITHUB_CLIENT_ID,
     clientSecret: process.env.GITHUB_CLIENT_SECRET,
   },
-  function(accessToken, refreshToken, profile, done) {
-    myDataBase.findOne({ username: profile.id }, function(err, user) {
-      if (err) {
-        done(err);
-      } else if (user) {
-        done(null, user);
-      } else {
-        const hash = bcrypt.hashSync('password', 12);
-        myDataBase.insertOne({
-          username: profile.id,
-          password: hash
-        },
-          (err, doc) => {
-            if (err) {
-              done(err)
-            } else {
-              // The inserted document is held within
-              // the ops property of the doc
-              done(null, doc.ops[0]);
-            }
-          }
-        )
-      }
-    })
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile);
+    //Database logic here with callback containing our user object
   }
 ));
 
