fc7d0bd9121b8a0795f0c56643ed4d93da6fe0a3
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2021-09-30 17:41:54 +0800

    Implementation of Social Authentication II

--- auth.js
+++ auth.js
@@ -35,11 +35,34 @@ module.exports = function (app, myDataBase) {
   passport.use(new GitHubStrategy({
     clientID: process.env.GITHUB_CLIENT_ID,
     clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: process.env.BASE_URL + "auth/github/callback",
   },
-  function(accessToken, refreshToken, profile, cb) {
-    console.log(profile);
-    //Database logic here with callback containing our user object
+  function(accessToken, refreshToken, profile, done) {
+    myDataBase.findOne({ username: profile.username }, function(err, user) {
+      if (err) {
+        done(err);
+      } else if (user) {
+        done(null, user);
+      } else {
+        const hash = bcrypt.hashSync('password', 12);
+        myDataBase.insertOne({
+          username: profile.username,
+          password: hash
+        },
+          (err, doc) => {
+            if (err) {
+              done(err)
+            } else {
+              // The inserted document is held within
+              // the ops property of the doc
+              done(null, doc.ops[0]);
+            }
+          }
+        )
+      }
+    })
   }
-));
+    
+  ));
 
 }
