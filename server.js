0adbf7e7c4218eac2858b6b6d9328689a93da6e0
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2021-09-30 14:35:38 +0800

    How to Put a Profile Together

--- server.js
+++ server.js
@@ -80,7 +80,9 @@ myDB(async client => {
   app
     .route('/profile')
     .get(ensureAuthenticated, (req,res) => {
-      res.render('/profile');
+      res.render('/profile', {
+        username: req.user.username,
+      });
     });
 
 }).catch(e => {
