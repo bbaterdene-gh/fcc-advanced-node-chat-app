3edf07f15154a240535463a8b0c0b04f41268d1f
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2021-09-30 14:15:29 +0800

    Authentication Strategies

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const fccTesting = require('./freeCodeCamp/fcctesting.js');
 const session = require('express-session')
 const passport = require('passport')
 const ObjectID = require('mongodb').ObjectID
+const LocalStrategy = require('passport-local');
 
 const app = express();
 
@@ -51,6 +52,18 @@ myDB(async client => {
   });
 
   // Be sure to add this...
+  passport.use(new LocalStrategy(
+    function(username, password, done) {
+      myDataBase.findOne({ username: username }, function (err, user) {
+        console.log('User '+ username +' attempted to log in.');
+        if (err) { return done(err); }
+        if (!user) { return done(null, false); }
+        if (password !== user.password) { return done(null, false); }
+        return done(null, user);
+      });
+    }
+  ));
+
 }).catch(e => {
   app.route('/').get((req, res) => {
     res.render('index', { title: e, message: 'Unable to login' });
