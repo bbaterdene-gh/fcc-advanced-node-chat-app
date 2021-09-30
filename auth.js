57eea1102f07b9cfdcda4ae609377f8c667aa417
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2021-09-30 17:54:29 +0800

    Implementation of Social Authentication II

--- auth.js
+++ auth.js
@@ -35,7 +35,7 @@ module.exports = function (app, myDataBase) {
   passport.use(new GitHubStrategy({
     clientID: process.env.GITHUB_CLIENT_ID,
     clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    callbackURL: process.env.BASE_URL + "auth/github/callback",
+    callbackURL: "" + process.env.BASE_URL + "auth/github/callback",
   },
   function(accessToken, refreshToken, profile, done) {
     myDataBase.findOne({ username: profile.username }, function(err, user) {
