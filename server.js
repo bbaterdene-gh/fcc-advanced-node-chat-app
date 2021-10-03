eaac500945ac0485e4ab3023047709a805073554
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2021-10-03 20:14:14 +0800

    Authentication with Socket.IO

--- server.js
+++ server.js
@@ -10,11 +10,27 @@ const auth = require('./auth')
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+const passportSocketIo = require('passport.socketio')
+const cookieParser = require('cookie-parser')
+const MongoStore = require('connect-mongo')(session);
+const URI = process.env.MONGO_URI;
+const store = new MongoStore({ url: URI });
+
 let currentUsers = 0;
+io.use(
+  passportSocketIo.authorize({
+    cookieParser: cookieParser,
+    key: 'express.sid',
+    secret: process.env.SESSION_SECRET,
+    store: store,
+    success: onAuthorizeSuccess,
+    fail: onAuthorizeFail
+  })
+);
 
 io.on('connection', socket => {
   ++currentUsers;
-  console.log('A user has connected');
+  console.log(`user ${socket.request.user.username || socket.request.user.name} connected`);
   io.emit('user count', currentUsers);
 
   socket.on('disconnect', () => {
@@ -34,6 +50,8 @@ app.set('view engine', 'pug')
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: true,
+  store: store,
+  key: 'express.sid',
   saveUninitialized: true,
   cookie: { secure: false }
 }));
@@ -52,3 +70,15 @@ const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
   console.log('Listening on port ' + PORT);
 });
+
+function onAuthorizeSuccess(data, accept) {
+  console.log('successful connection to socket.io');
+
+  accept(null, true);
+}
+
+function onAuthorizeFail(data, message, error, accept) {
+  if (error) throw new Error(message);
+  console.log('failed connection to socket.io:', message);
+  accept(null, false);
+}
